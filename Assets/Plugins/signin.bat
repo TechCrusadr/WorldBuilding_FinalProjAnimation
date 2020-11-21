caffd0343268573b737dd8c4918aa4ecaa3a0961
Justin Chow <jjc2184@caa.columbia.edu>
2020-11-21 00:10:35 -0800

    override all to make prefab

---
+++ Assets/Plugins/signin.bat
@@ -0,0 +1,3 @@
+echo off
+title Sign In to AltspaceVR
+curl -v -d "user[email]=hohaddict@gmail.com&user[password]=AkyuuLuna88OYOW!E" https://account.altvr.com/users/sign_in.json -c cookie
---
+++ Assets/Plugins/uploadKit.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "kit[zip]=@D:\UnityProjects\2020_WorldBuildingClass\FinalProject-Animations\1610757709777338584_wbr3finalassignment.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1610757709777338584.json
